799e82bda5fe93a75aeb17c201e5cafe06f9640a
MarcusRoncolato3021 <marcus_kitos@hotmail.com>
2025-04-16 17:23:42 -0300

    Corrigido problema com a barra de status em iOS usando black-translucent e env(safe-area-inset-top)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       <head>
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="theme-color" content="#e6f1ff" />
       </head>
       <body className={inter.className}>
